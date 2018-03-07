3b72daf67059fde55bc446167fbb0e8f1a30a515
Elliot Bonneville <elliot.bonneville@gmail.com>
2018-03-07 15:54:07 -0500

    Improve Dijkstra algorithm slightly

--- src/js/model/features/level/dijkstraMap.selector.js
+++ src/js/model/features/level/dijkstraMap.selector.js
@@ -1,3 +1,6 @@
+import { MAP_WIDTH, MAP_HEIGHT } from '~/constants';
+
+import rectangle from '~/utils/rectangle';
 import requiredProp from '~/utils/requiredProp';
 import cellKey from '~/utils/cellKey';
 
@@ -10,13 +13,22 @@ import {
 } from '~/model/features/level/selectors';
 import { getItemsWithValues } from '~/model/features/items/selectors';
 
+const unassignedDistanceMap = rectangle({
+    width: MAP_WIDTH,
+    height: MAP_HEIGHT
+}).reduce(
+    (map, coords) => Object.assign( map, { [cellKey(coords)]: Infinity }),
+    {},
+);
+
 const generateMap = ({
     state = requiredProp('state'),
     pointsOfInterest = requiredProp('pointsOfInterest'),
     maxInterest = requiredProp('maxInterest'),
     tiles = requiredProp('tiles'),
 }) => {
-    const combinedDistances = {};
+    const startTime = Date.now();
+    const combinedDistances = Object.assign({}, unassignedDistanceMap);
     const graphs = pointsOfInterest.map((point) => {
         const frontier = [point.position];
         const distance = { [point.position]: 0 };
@@ -29,14 +41,24 @@ const generateMap = ({
                 if (typeof distance[position] === 'undefined') {
                     frontier.push(position);
                     distance[position] = distance[current] + 1;
-                    combinedDistances[position] = Math.max(
-                        combinedDistances[position] || 0,
-                        distance[position],
-                    );
+                    combinedDistances[position] = tiles[position].data.walkable
+                        ? Math.min(
+                            combinedDistances[position],
+                            distance[position],
+                        )
+                        : Infinity;
                 }
             });
         }
     });
+
+    if (game.debug) {
+        const time = Date.now() - startTime;
+        const points = pointsOfInterest.length;
+        console.log(
+            `Took ${time}ms to generate map with ${points} points of interest`,
+        );
+    }
 
     return combinedDistances;
 };
---
+++ src/js/view/components/data/DijkstraMapView.js
@@ -0,0 +1,38 @@
+import DataView from '~/view/components/data/DataView';
+import Cell from '~/view/Cell';
+
+import dijkstraMap from '~/model/features/level/dijkstraMap.selector';
+
+const valueColors = [
+    'E50500',
+    'CE0A13',
+    'B71026',
+    'A01639',
+    '891C4C',
+    '72225F',
+    '5B2772',
+    '442D85',
+    '2D3398',
+    '1639AB',
+    '003FBF',
+];
+
+export default () => DataView({
+    selectStateData: state => dijkstraMap({ state, name: 'items' }),
+    renderData: values => (!game.debug) ? {} : Object.entries(values).reduce(
+        (cells, [coordinates, value]) => {
+            if (value === Infinity) return cells;
+            const shortValue = (value > 9) ? 9 : value;
+            return Object.assign(
+                cells, 
+                {
+                    [coordinates]: new Cell({
+                        character: shortValue,
+                        backgroundColor: valueColors[shortValue],
+                    })
+                },
+            );
+        },
+        {},
+    ),
+});
