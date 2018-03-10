cd0fdec5d7bfea1fbe43081d302e3cd0169fae93
Elliot Bonneville <elliot.bonneville@gmail.com>
2018-03-10 13:31:30 -0500

    Don't give up if there's still an actor on the level

--- src/js/model/features/stats/listeners.js
+++ src/js/model/features/stats/listeners.js
@@ -1,6 +1,7 @@
 import { getMana } from './selectors';
 import { getValueOfItemsOnFloor } from '~/model/features/items/selectors';
 import { getGameStatus } from '~/model/features/status/selectors';
+import { getActors } from '~/model/features/actors/selectors';
 
 import lose from '~/model/features/status/lose.action';
 
@@ -13,11 +14,16 @@ export default ({ store }) => {
 
         const manaRemaining = getMana(state);
         const treasureRemaining = getValueOfItemsOnFloor(state);
+        const actors = getActors(state);
 
         // If there is no treasure left to generate power, and the total power
         // is less than the cheapest treasure, there is no way to get more,
         // so...  game ends because there's nothing the player can do anymore.
-        if (treasureRemaining === 0 && manaRemaining < 5) {
+        if (
+            treasureRemaining === 0 &&
+            manaRemaining < 5 &&
+            Object.values(actors).length === 0
+        ) {
             setTimeout(() => {
                 store.dispatch(
                     lose('You ran out of treasure and power... Game Over.'),
